5a86086f37aa8ee944b1f32aeb81e8f45f4fbd26
LucindaHe <lucinda52152@gmail.com>
2024-04-21 18:45:12 +0800

    Initial commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/replace_word/'
-    : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/replace_word/" : "/",
+};
